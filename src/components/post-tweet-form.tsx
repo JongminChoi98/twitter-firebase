3215682b7ae656ee9481624e8aa0439556569176
JongminChoi98 <joeychoi7624@gmail.com>
2023-12-25 00:54:32 -0800

    [ADD] post-tweets

---
+++ src/components/post-tweet-form.tsx
@@ -0,0 +1,157 @@
+import { useState } from "react";
+import { styled } from "styled-components";
+import { Error } from "./auth-components";
+import { FirebaseError } from "firebase/app";
+import { auth, db, storage } from "../firebase";
+import { addDoc, collection, updateDoc } from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+
+const Wrapper = styled.div``;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const TextArea = styled.textarea`
+  border: 2px solid white;
+  padding: 20px;
+  border-radius: 20px;
+  font-size: 16px;
+  color: white;
+  background-color: black;
+  width: 100%;
+  resize: none;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  &::placeholder {
+    font-size: 16px;
+  }
+  &:focus {
+    outline: none;
+    border-color: #1d9bf0;
+  }
+`;
+
+const AttachFileButton = styled.label`
+  padding: 10px 0px;
+  color: #1d9bf0;
+  text-align: center;
+  border-radius: 20px;
+  border: 1px solid #1d9bf0;
+  font-size: 14px;
+  font-weight: 600;
+  cursor: pointer;
+`;
+
+const AttachFileInput = styled.input`
+  display: none;
+`;
+
+const SubmitBtn = styled.input`
+  background-color: #1d9bf0;
+  color: white;
+  border: none;
+  padding: 10px 0px;
+  border-radius: 20px;
+  font-size: 16px;
+  cursor: pointer;
+  &:hover,
+  &:active {
+    opacity: 0.9;
+  }
+  &:disabled {
+    opacity: 0.3;
+  }
+`;
+
+export default function PostTweetForm() {
+  const [isLoading, setLoading] = useState(false);
+  const [tweet, setTweet] = useState("");
+  const [file, setFile] = useState<File | null>(null);
+  const [error, setError] = useState("");
+  const [disable, setDisable] = useState(false);
+
+  const maxFileSizeInMB = 1;
+  const maxFileSizeInKB = 1024 * 1024 * maxFileSizeInMB;
+
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTweet(e.target.value);
+  };
+  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if (files && files.length === 1 && files[0].size < maxFileSizeInKB) {
+      setFile(files[0]);
+    }
+    if (files && files[0].size > maxFileSizeInKB) {
+      alert(`Please select a file that is ${maxFileSizeInMB}MB or less.`);
+      setDisable(true);
+    } else {
+      setDisable(false);
+    }
+  };
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError("");
+    const user = auth.currentUser;
+    if (!user || isLoading || tweet === "" || tweet.length > 180) return;
+    try {
+      setLoading(true);
+      const doc = await addDoc(collection(db, "tweets"), {
+        tweet,
+        createdAt: Date.now(),
+        username: user.displayName || "Anonymous",
+        userId: user.uid,
+      });
+      if (file) {
+        const locationRef = ref(
+          storage,
+          `tweets/${user.uid}-${user.displayName}/${doc.id}`
+        );
+        const result = await uploadBytes(locationRef, file);
+        const url = await getDownloadURL(result.ref);
+        await updateDoc(doc, {
+          photo: url,
+        });
+      }
+      setTweet("");
+      setFile(null);
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        setError(e.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <Wrapper>
+      <Form onSubmit={onSubmit}>
+        <TextArea
+          required
+          rows={5}
+          maxLength={180}
+          onChange={onChange}
+          value={tweet}
+          placeholder="Post here!"
+        />
+        <AttachFileButton htmlFor="file">
+          {file ? "Photo added ✅" : "Add photo"}
+        </AttachFileButton>
+        <AttachFileInput
+          onChange={onFileChange}
+          type="file"
+          id="file"
+          accept="image/*"
+        />
+        <SubmitBtn
+          type="submit"
+          value={isLoading ? "Posting..." : "Post Tweet"}
+          disabled={disable}
+        />
+      </Form>
+      {error !== "" ? <Error>{error}</Error> : null}
+    </Wrapper>
+  );
+}
