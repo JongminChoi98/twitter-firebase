709a9927a933de4884212c76af73dac51e909010
JongminChoi98 <joeychoi7624@gmail.com>
2024-01-08 10:26:27 -0800

    [ADD] photo remove button while editing

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { ITweet } from "./timeline";
 import { auth, db, storage } from "../firebase";
-import { deleteDoc, doc } from "firebase/firestore";
+import { deleteDoc, deleteField, doc, updateDoc } from "firebase/firestore";
 import { deleteObject, ref } from "firebase/storage";
 import EditTweetForm from "./edit-tweet-form";
 import { useState } from "react";
@@ -71,6 +71,23 @@ const EditButton = styled.button`
   cursor: pointer;
 `;
 
+const DeletePhoto = styled.button`
+  background: none;
+  color: inherit;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  margin-top: 10px;
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
 export default function Tweet({ userId, username, photo, tweet, id }: ITweet) {
   const [isEditing, setIsEditing] = useState(false);
   const user = auth.currentUser;
@@ -85,6 +102,19 @@ export default function Tweet({ userId, username, photo, tweet, id }: ITweet) {
   };
 
   const onEdit = () => setIsEditing((prev) => !prev);
+
+  const onDeletePhoto = async () => {
+    const ok = confirm("Are you sure to delete?");
+
+    if (!ok || user?.uid !== userId) return;
+
+    const tweetRef = doc(db, "tweets", id);
+    await updateDoc(tweetRef, {
+      photo: deleteField(),
+    });
+    await deleteObject(ref(storage, `tweets/${user.uid}/${id}`));
+    onEdit();
+  };
 
   return (
     <Wrapper>
@@ -112,6 +142,18 @@ export default function Tweet({ userId, username, photo, tweet, id }: ITweet) {
       {photo ? (
         <Column>
           <Photo src={photo} />
+          {isEditing ? (
+            <DeletePhoto onClick={onDeletePhoto}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+              </svg>
+            </DeletePhoto>
+          ) : null}
         </Column>
       ) : null}
     </Wrapper>
